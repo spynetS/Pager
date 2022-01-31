a14bb91fce616cc6c50826a8377a0e640ca45e07
Alfred <alfred@stensatter.se>
2022-01-31 01:16:55 +0100

    update

--- Pager_Project/js/navigation.js
+++ Pager_Project/js/navigation.js
@@ -1,79 +1,56 @@
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-function changePage(index,display){
-    document.cookie="page="+index;
-    var pages = []
-    var arrayLength = pages.length;
-    if(pages.includes(index.toString()))
+
+class Component
+{
+    constructor(props)
     {
-        for(var i = 0;i<arrayLength;i++)
-        {
-            if(pages[i]===index)
-            {
-                document.getElementById(pages[i]).style.display = display;
-            }
-            else if(pages[i]==index)
-            {
-                document.getElementById(pages[i]).style.display = display;
-            }
-            else
-            {
-                document.getElementById(pages[i]).style.display = "none";
-            }
-        }
-    }
-}
-function changeMasterPage(index,display){
-    
-    if(index.toString().includes("main"))
-    {
-        document.cookie="masterpage="+index;
-    }
-    else if(index >0)
-    {
-        document.cookie="masterpage=main"+index;
-        index = "main"+index.toString();
-    }
-    else
-    {
-        document.cookie="masterpage=main0";
-        index = "main0";
-    }
-    var mainpages = []
-    var arrayLength1 = mainpages.length;
-    if(mainpages.includes(index.toString()))
-    { 
-        for(var i = 0;i<arrayLength1;i++)
-        {
-            if(mainpages[i]===index)
-            {
-                document.getElementById(mainpages[i]).style.display = display;
-            }
-            else
-            {
-                document.getElementById(mainpages[i]).style.display = "none";
-            }
+
+        this.props={
+
         }
     }
 }
 
-function reload()
+
+class Page1 
 {
-    var pageindex = getCookie("page");
-    var masterpageindex = getCookie("masterpage");
-    changePage(pageindex);
-    changeMasterPage(masterpageindex);
+    
+    constructor(props)
+    {
+        this.props = props;
+        console.log(props);
+    }
+
+
+
+    render()
+    {
+        return('<div><h1>'+this.props.name+'</h1></div>');
+
+    }
+}
+
+class Page2
+{
+    
+    constructor(props)
+    {
+        this.props = props;
+        this.state ={
+            number:1
+        }
+    }
+
+    test()
+    {
+        this.state.number += 1;
+    }
+
+    render()
+    {
+        let p = new Page1({name:"wtf"});
+        document.write('<div><h2>'+this.state.number+'</h2><button onclick="test()" >increase</button></div>')
+
+//        return('<div><h2>'+this.state.number+'</h2><button onclick="test()" >increase</button></div>');
+            
+    }
 }
